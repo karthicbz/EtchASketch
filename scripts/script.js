09c8125c17f0f238949ac85e8eb06255eb2e791c
Karthick <pkarthicbz@live.in>
2022-10-20 21:48:29 +0530

    add color picker button to change colors

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector('.container');
 // const childdiv = document.querySelectorAll('.childdiv');
+const colorPicker = document.querySelector('.colorPicker');
+let color = 'black';
 const width = 500/64;
 const height = 500/64;
 let isToggle = false;
+
+colorPicker.addEventListener('click', (e)=>{
+    color = prompt('enter the color name'); 
+});
 
 for(let i=0; i<64; i++){
   for(let j=0; j<64; j++){
@@ -27,7 +33,8 @@ function mouseUp(){
 childdiv.forEach(div=>div.addEventListener('mouseenter', (e)=>{
   e.target.addEventListener('mousedown', mouseDown);
   if(isToggle){
-    e.target.classList.add('style');
+    // e.target.classList.add('style');
+    e.target.style.backgroundColor = `${color}`;
   }
   e.target.addEventListener('mouseup', mouseUp);
   // console.log(e);
